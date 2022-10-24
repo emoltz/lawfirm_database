428bd613adb6566b979178ae73f9992d11bc6aec
emoltz <es5541@nyu.edu>
2022-10-24 14:59:52 -0400

    fun animation

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import psycopg2
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Test", page_icon=":smile:", layout='wide')
+
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_w51pcehl.json")
 
 with st.container():
     st.subheader("Hi, I am Ethan :wave:")
@@ -17,3 +29,4 @@ with st.container():
         st.write("Left column")
     with right_column:
         st.write("Right column")
+        st_lottie(lottie_coding)
