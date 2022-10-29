64608c750d0c1b2e54a6028d13c46b006ea229f5
emoltz <es5541@nyu.edu>
2022-10-29 14:51:27 -0400

    reading sql query

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -1,19 +1,26 @@
 import streamlit as st
+import pandas as pd
+import psycopg2
 
 header = st.container()
 dataset = st.container()
 features = st.container()
 model_training = st.container()
 
+conn = psycopg2.connect(**st.secrets["postgres"])
+cur = conn.cursor()
+data = pd.read_sql_query('SELECT * FROM lawyers', conn)
+
 with header:
     st.title("Welcome!")
     st.text('In this project, I will look into transactions of taxis in NYC')
 
-
 with dataset:
     # dataset section
-    st.header("NYC Taxi Dataset")
+    st.header("Fake Lawyer Dataset")
     st.text("I found this dataset on blahblahblah")
+    # data = pd.read_csv('sample_data/sample_data.csv')
+    st.write(data)
 
 with features:
     # section
