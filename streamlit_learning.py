89f342f2d1cf3dcc3aaf4ccda716fef771310a7e
emoltz <es5541@nyu.edu>
2022-10-29 15:52:34 -0400

    data displayed!

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -7,9 +7,23 @@ dataset = st.container()
 features = st.container()
 model_training = st.container()
 
-conn = psycopg2.connect(**st.secrets["postgres"])
-cur = conn.cursor()
-data = pd.read_sql_query('SELECT * FROM lawyers', conn)
+
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+
+conn = init_connection()
+
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+
+data = pd.DataFrame(run_query("SELECT * from lawyers;")).rename(columns={0: "lid", 1: "firstname", 2: "lastname", 3: "title", 4: "email", 5: "specialty", 6: "rate_per_hour"})
 
 with header:
     st.title("Welcome!")
