078cccd00659a407e481fa851c56d31cbdbbb5d4
emoltz <es5541@nyu.edu>
2022-10-29 11:50:15 -0400

    moved into functions

--- fake_data_gen.py
+++ fake_data_gen.py
@@ -2,7 +2,7 @@ from faker import Faker
 import streamlit as st
 import psycopg2
 
-
+# CLASSES:
 class FakeLawyer:
     lid = 0
     firstname = ""
@@ -22,25 +22,51 @@ class FakeLawyer:
         self.specialty = fake.word()
         self.rate_per_hour = fake.random_int(min=1, max=500)
 
-
 class FakeLawfirm:
     list_of_lawyers = []
 
     def __init__(self):
-        for _ in range(10):
+        for _ in range(25):
             self.list_of_lawyers.append(FakeLawyer())
 
     def print_lawyers(self):
         for lawyer in self.list_of_lawyers:
             print(lawyer.firstname, lawyer.lastname, lawyer.title, lawyer.email, lawyer.specialty, lawyer.rate_per_hour)
 
+class FakeClient:
+    cid = 0
+    firstname = ""
+    lastname = ""
+    email = ""
+    phone = ""
 
-lawfirm = FakeLawfirm()
-# lawfirm.print_lawyers()
+    def __init__(self):
+        pass
+
 
 conn = psycopg2.connect(**st.secrets["postgres"])
 cur = conn.cursor()
-for lawyer in lawfirm.list_of_lawyers:
-    cur.execute(
-        f"INSERT INTO lawyers (lid, firstname, lastname, title, email, specialty, rate_per_hour) VALUES ('{lawyer.lid}', '{lawyer.firstname}', '{lawyer.lastname}', '{lawyer.title}','{lawyer.email}','{lawyer.specialty}','{lawyer.rate_per_hour}')")
-    conn.commit()
+
+
+
+# FUNCTIONS:
+def populate_laywer_table():
+    lawfirm = FakeLawfirm();
+    for lawyer in lawfirm.list_of_lawyers:
+        cur.execute(
+            f"INSERT INTO lawyers (lid, firstname, lastname, title, email, specialty, rate_per_hour) VALUES ('{lawyer.lid}', '{lawyer.firstname}', '{lawyer.lastname}', '{lawyer.title}','{lawyer.email}','{lawyer.specialty}','{lawyer.rate_per_hour}')")
+        conn.commit()
+
+
+
+
+
+
+
+
+
+# SCRIPTS:
+populate_laywer_table();
+
+
+
